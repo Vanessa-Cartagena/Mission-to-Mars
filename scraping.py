be9a8f052d880df037c03432c83a8f5fa1eb91b3
Vanessa-Cartagena <vcartagena09@gmail.com>
2022-08-21 14:31:50 -0400

    module 10 Challenge

--- scraping.py
+++ scraping.py
@@ -1,10 +1,9 @@
 # Import Splinter, BeautifulSoup, and Pandas
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
+from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 import datetime as dt
-from webdriver_manager.chrome import ChromeDriverManager
-
 
 def scrape_all():
     # Initiate headless driver for deployment
@@ -19,9 +18,9 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now(),
-        "hemispheres": hemispheres(browser)
-    }
+        "hemispheres": hemispheres(browser),
+        "last_modified": dt.datetime.now()
+        }
 
     # Stop webdriver and return data
     browser.quit()
@@ -98,7 +97,7 @@ def mars_facts():
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
 
-def hemishperes(browser):
+def hemispheres(browser):
     # 1. Use browser to visit the URL 
     url = 'https://marshemispheres.com/'
     browser.visit(url)
